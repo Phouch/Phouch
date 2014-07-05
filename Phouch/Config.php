4033e8570fca1098a7f5dd258c0a6637b8b1ba90
Dustin Moorman <dustin.moorman@gmail.com>
2014-07-05 11:13:56 -0500

    notes

--- Phouch/Config.php
+++ Phouch/Config.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Phouch\Config
+ * @author Dustin Moorman <dustin.moorman@gmail.com>
+ * @description Master configuration for this Phouch
+ * instance. Will contain the following (a living list
+ * until version 1.0.0):
+ *
+ * - Default HTTP Option Object
+ *
+ */
 
 namespace Phouch;
 
