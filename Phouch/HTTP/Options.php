18db75521a3af4682185f84ab5091f99943d2bbf
Dustin Moorman <dustin.moorman@gmail.com>
2014-07-06 18:09:19 -0500

    setting up some HTTP\Options goodness

--- Phouch/HTTP/Options.php
+++ Phouch/HTTP/Options.php
@@ -3,5 +3,17 @@
 namespace Phouch\HTTP;
 
 class Options {
+  private $host;
+  private $port;
+  private $transport;
+
+  public function __construct(){
+    return $this;
+  }
+
+  public function setHost($host){
+    $this->host = $host;
+    return $this;
+  }
 
 }
