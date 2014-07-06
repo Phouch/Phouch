fdac1d1e2d892620a4c7262709a02636cd0b9b50
Dustin Moorman <dustin.moorman@gmail.com>
2014-07-06 18:39:08 -0500

    adding port exception class

---
+++ Phouch/Exception/HTTP/Port.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Phouch\Exception\HTTP;
+
+class Port extends \Phouch\Exception\Base {
+  public function __construct($port){
+    $message = $port . ' is not a valid port number.';
+    parent::__construct($message);
+  }
+}
