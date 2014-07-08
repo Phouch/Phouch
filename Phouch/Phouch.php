49bcec2071bf268948cae02a7b727266d31090d5
Dustin Moorman <dustin.moorman@gmail.com>
2014-07-07 21:40:17 -0500

    notes & docblock

--- Phouch/Phouch.php
+++ Phouch/Phouch.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Phouch\Phouch
+ * @author Dustin Moorman <dustin.moorman@gmail.com>
+ * The Phouch object is sort of like the "Entity Manager" in
+ * Doctrine 2. It is responsible for pushing Phouch\Model's to
+ * persistence.
+ *
+ * Note: Top level classes in Phouch\Phouch are instantiator
+ * classes for more complex objects, ie - facades that safely
+ * create a class based on how you will use it.
+ */
 
 namespace Phouch;
 
