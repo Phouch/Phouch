036d954931ee0d4717848da53c5f8b2ffa3e9688
Dustin <dustin.moorman@gmail.com>
2014-07-19 14:17:12 -0500

    Rollin

--- Phouch/HTTP/Response.php
+++ Phouch/HTTP/Response.php
@@ -1,8 +1,21 @@
 <?php
+/**
+ * Phouch\HTTP\Response
+ * @description Response object from HTTP requests, a
+ * product of the HTTP\Request execution.
+ */
 
 namespace Phouch\HTTP;
 
 class Response {
+
+    /**
+     * @description Takes either an array with key 'error'
+     * and value of the Exception's message, or the array
+     * provided from CouchDB after execution of an
+     * HTTP\Request.
+     * @param array $response
+     */
     public function __construct(array $response){
 
     }
