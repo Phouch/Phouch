fc03458e67e8c9e90cdd81e1d61a31527bd3a0a6
Dustin <dustin.moorman@gmail.com>
2014-07-19 11:27:38 -0500

    conditionally set options at constructor if supplied

--- Phouch/HTTP/Request/Base.php
+++ Phouch/HTTP/Request/Base.php
@@ -9,8 +9,14 @@ class Base {
 	/** @var \Phouch\HTTP\Options */
 	protected $_options;
 
-    public function __construct(\Phouch\HTTP\Options $options){
+    public function __construct(){
     	if(!isset($this->_curl_handle))
     		$this->_curl_handle = curl_init();
+        if(func_num_args() > 0){
+            $arg0 = func_get_arg(0);
+            if($arg0 instanceof \Phouch\HTTP\Options){
+                $this->_options = $arg0;
+            }
+        }
     }
 }
