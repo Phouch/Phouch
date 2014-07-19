966cb6ea49bb7d0b793a7bfb10d13d3865c9061c
Dustin <dustin.moorman@gmail.com>
2014-07-19 11:23:20 -0500

    setting up

--- Phouch/HTTP/Request/Base.php
+++ Phouch/HTTP/Request/Base.php
@@ -6,10 +6,10 @@ class Base {
 
 	protected $_curl_handle;
 
-	/** @var Phouch\HTTP\Options */
+	/** @var \Phouch\HTTP\Options */
 	protected $_options;
 
-    public function __construct(Options $options){
+    public function __construct(\Phouch\HTTP\Options $options){
     	if(!isset($this->_curl_handle))
     		$this->_curl_handle = curl_init();
     }
