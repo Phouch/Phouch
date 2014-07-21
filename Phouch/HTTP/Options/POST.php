08bc1e762794e04a8b76bc0b5b174b17a4e89214
Dustin <dustin.moorman@gmail.com>
2014-07-21 12:15:38 -0500

    capitalizing POST

---
+++ Phouch/HTTP/Options/POST.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Phouch\HTTP\Options;
+
+class POST extends Base {
+
+    private $postData = array();
+
+    public function __construct(){
+        $this->method = 'POST';
+    }
+
+    public function setPostData(array $data){
+        $this->postData = $data;
+        return $this;
+    }
+
+    public function getPostData(){
+        return $this->postData;
+    }
+}
