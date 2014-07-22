b51dfde4b4da27380c5f146a422378b974ffcdb4
Dustin Moorman <dustin.moorman@gmail.com>
2014-07-22 13:00:16 -0500

    Phouch\HTTP\Service\Crl => Phouch\HTTP\Service\Curl

--- config/Phouch.php
+++ config/Phouch.php
@@ -7,7 +7,7 @@ return array(
     "cert_file_path" => "path/to/my/cert.file",
     "transport" => "https",
     "http_service_providers" => array(
-        "Phouch_Curl" => "Phouch\HTTP\Service\Crl" 
+        "Phouch_Curl" => "Phouch\HTTP\Service\Curl" 
     ),
     "http_service_provider" => "Phouch_Curl"
 );
