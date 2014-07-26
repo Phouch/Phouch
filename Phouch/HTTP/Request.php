d1965376c875f925628f64487f37614b35f9e471
Dustin Moorman <dustin.moorman@gmail.com>
2014-07-26 12:38:04 -0500

    ZF2 code standards compliance

--- Phouch/HTTP/Request.php
+++ Phouch/HTTP/Request.php
@@ -17,29 +17,30 @@
  */
 namespace Phouch\HTTP;
 
-class Request {
+class Request
+{
+    /** @var  \Phouch\HTTP\Service\HttpServiceInterface */
+    protected $_httpService;
 
-    /** @var  \Phouch\HTTP\Service\HttpService */
-    protected $_http_service;
+  	/** @var \Phouch\HTTP\Options\OptionsAbstract */
+	  protected $_options;
 
-	/** @var \Phouch\HTTP\Options\Base */
-	protected $_options;
-
-    public function __construct(){
+    public function __construct()
+    {
         if(func_num_args() > 0)
             $this->setComponentsIfPassed(func_get_args());
     }
 
-    public function execute(){
-
+    public function execute()
+    {
         if(func_num_args() > 0)
             $this->setComponentsIfPassed(func_get_args());
 
         try {
-            if(!$this->_options instanceof Options\Base)
+            if(!$this->_options instanceof Options\OptionsAbstract)
                 throw new \Phouch\Exception\HTTP\Options\NotSet();
 
-            if(!$this->_http_service instanceof Service\HttpService)
+            if(!$this->_httpService instanceof Service\HttpServiceInterface)
                 throw new \Phouch\Exception\HTTP\Service\NotSet();
 
         } catch(\Exception $e){
@@ -48,32 +49,34 @@ class Request {
             );
         }
 
-        $this->_http_service->setOptions($this->_options);
+        $this->_httpService->setOptions($this->_options);
 
         return new Response(
-            $this->_http_service->execute()
+            $this->_httpService->execute()
         );
 
     }
 
-    public function setOptions(Options\Base $options){
+    public function setOptions(Options\OptionsAbstract $options)
+    {
         $this->_options = $options;
         return $this;
     }
 
-    public function setHTTPService(Service\HttpService $http_service){
-        $this->_http_service = $http_service;
+    public function setHTTPService(Service\HttpServiceInterface $httpService)
+    {
+        $this->_httpService = $httpService;
         return $this;
     }
 
-    private function setComponentsIfPassed($components){
+    private function setComponentsIfPassed($components)
+    {
         foreach($components as $component){
-            if($component instanceof Options\Base)
+            if($component instanceof Options\OptionsAbstract)
                 $this->setOptions($component);
 
-            if($component instanceof Service\HttpService)
+            if($component instanceof Service\HttpServiceInterface)
                 $this->setHTTPService($component);
-
         }
     }
 }
