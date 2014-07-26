4b900794a19149fb8e3ded7f95515bdfb747e735
Dustin Moorman <dustin.moorman@gmail.com>
2014-07-26 12:39:44 -0500

    ZF2 code standards compliance

--- Phouch/Exception/Document/Value/NotExists.php
+++ Phouch/Exception/Document/Value/NotExists.php
@@ -2,8 +2,10 @@
 
 namespace Phouch\Exception\Document\Value;
 
-class NotExists extends \Phouch\Exception\Base {
-    public function __construct(){
+class NotExists extends \Phouch\Exception\ExceptionAbstract
+{
+    public function __construct()
+    {
         $message = 'Document value does not exist.';
         parent::__construct($message);
     }
