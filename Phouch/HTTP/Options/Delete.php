b792f1dc5dfbfe177b536e1ebb1928f7ca7475df
Dustin Moorman <dustin.moorman@gmail.com>
2014-07-26 12:21:07 -0500

    ZF2 conformance

--- Phouch/HTTP/Options/Delete.php
+++ Phouch/HTTP/Options/Delete.php
@@ -2,8 +2,10 @@
 
 namespace Phouch\HTTP\Options;
 
-class Delete extends Base {
-    public function __construct(){
+class Delete extends OptionsAbstract
+{
+    public function __construct()
+    {
         $this->method = 'DELETE';
     }
 }
