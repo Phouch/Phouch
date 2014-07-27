611a5a6c1400300df784d859dc51509fdb63c5ed
Dustin Moorman <dustin.moorman@gmail.com>
2014-07-27 15:22:21 -0500

    refactoring to Payload in lieu of PostData, since applies for POST or PUT

--- Phouch/HTTP/Options/Put.php
+++ Phouch/HTTP/Options/Put.php
@@ -4,8 +4,21 @@ namespace Phouch\HTTP\Options;
 
 class Put extends OptionsAbstract
 {
+    private $_payload = array();
+
     public function __construct()
     {
         $this->_method = 'PUT';
+    }
+
+    public function setPayload(array $data)
+    {
+        $this->_payload = $data;
+        return $this;
+    }
+
+    public function getPayload()
+    {
+        return $this->_payload;
     }
 }
