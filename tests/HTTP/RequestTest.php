efa3157c55082385c0b636e80164bc390e064e50
Dustin Moorman <dustin.moorman@gmail.com>
2014-08-01 15:23:36 -0500

    two assertions

--- tests/HTTP/RequestTest.php
+++ tests/HTTP/RequestTest.php
@@ -5,6 +5,8 @@ class RequestTest extends PHPUnit_Framework_TestCase {
     public function testUnconfiguredExecution(){
         $req = new \Phouch\HTTP\Request();
         $response = $req->execute();
+
         $this->assertTrue($response->hasError());
+        $this->assertEquals('HTTP Options not set.', $response->getError());
     }
 }
