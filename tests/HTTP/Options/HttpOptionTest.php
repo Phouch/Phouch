4ac0c2f44ecbeb3f24f1412e4e30acd934dabd64
Dustin Moorman <dustin.moorman@gmail.com>
2014-08-03 21:38:04 -0500

    adding HttpOptionTest and testSetWithArrayByConstruct

---
+++ tests/HTTP/Options/HttpOptionTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class HttpOptionTest extends PHPUnit_Framework_TestCase {
+    public function testSetWithArrayByConstruct(){
+        $httpPOST = new \Phouch\HTTP\Options\Post(array(
+            'host' => 'tau.pe',
+            'port' => 80
+        ));
+
+        $this->assertEquals('tau.pe',$httpPOST->getHost());
+
+    }
+}
